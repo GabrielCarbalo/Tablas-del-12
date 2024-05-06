b1deac333e89e186005426a079440a0c566f698b
GabrielCarbalo <164204548+GabrielCarbalo@users.noreply.github.com>
2024-05-06 12:32:18 -0600

    Add files via upload

---
+++ tablas.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int i = 1;
+    while (i<13)
+    {
+        
+        cout << "12"<< i << " = " << 12*i <<endl;
+        i++;
+
+    }
+
+    
+    return 0;
+}
